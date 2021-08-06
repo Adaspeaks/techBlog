0e65f579e937f8fec69bcc92f06aee1767345063
Adaspeaks <adaspeaks@gmail.com>
2021-08-06 02:32:33 -0700

    created seeds to test posts and users

---
+++ seeds/postData.js
@@ -0,0 +1,27 @@
+[
+    {
+        
+        "title": "Why am I so tired?", 
+        "post_body": "Been up for way too long"
+    },
+    {
+        
+        "title": "Haven't slept in days", 
+        "post_body": "Thank god for coffee"
+    },
+    {
+        
+        "title": "What's sleep?", 
+        "post_body": "I've never heard of it before"
+    },
+    {
+        
+        "title": "None of you are funny", 
+        "post_body": "But neither am I"
+    },
+    {
+        
+        "title": "You ALL have insomnia.", 
+        "post_body": "Wake up PEOPLE!... or actually maybe don't"
+    }
+]
---
+++ seeds/userData.js
@@ -0,0 +1,14 @@
+[
+    {
+        "username": "Tied", 
+        "password": "very"
+    },
+    {
+        "username" : "Johnny",
+        "password" : "generic"
+    },
+    {
+        "username" : "username",
+        "password" : "password"
+    }
+]
